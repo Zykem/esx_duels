adff85f62905d6462ad3a583934a17fe3192830b
zykem <86602828+Zykem@users.noreply.github.com>
2024-12-29 20:45:20 +0100

    feat(client/main.lua) Add invite expiry
    Added expiring of invite after 10 seconds of no response

--- client/main.lua
+++ client/main.lua
@@ -8,10 +8,20 @@ local function sendDuelInvite(invitedBy, inivitedByName)
         { title = 'Decline', value = 'decline' }
     }
 
+    local inviteExpired = false
+
     ESX.OpenContext('right', elements, function(menu, element)
+        if inviteExpired then
+            TriggerServerEvent('esx_duels:declineDuelInvite', invitedBy)
+            return
+        end
         local event = ('esx_duels:%sDuelInvite'):format(element.value)
         TriggerServerEvent(event, invitedBy)
         ESX.CloseContext()
+    end)
+
+    SetTimeout(10000, function()
+        inviteExpired = true
     end)
 end
 
